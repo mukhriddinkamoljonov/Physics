61cecf18aa58f387313120decc7146ec2bc4bc2a
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-19 22:35:16 +0500

    togirlandi

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Navbar from "./Components/Navbar/Navbar";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
