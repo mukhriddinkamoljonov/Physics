fa808964140c1b1093b3a9cda6fb703b57e5bcd3
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-19 18:07:54 +0500

    changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <h1>Hello</h1>
     </div>
   );
 }
