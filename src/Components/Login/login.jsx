f3d11c115dd672c8fd8c0fa2333f9d6b23fe3333
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-22 22:30:26 +0500

    changed

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [wait, setWait] = useState(!false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWait(true);
+    console.log("submit");
+  };
+
   return (
-    <div className="form-container">
-      <form>
+    <div className="form-container" disabled>
+      <form onSubmit={handleSubmit}>
         <h1 className="form-title">Ro'yxatdan O'tish</h1>
         <input type="text" placeholder="Ismingizni kiriting" /> <br />
         <input type="text" placeholder="Familyangizni kiriting" />
         <br />
         <input type="password" placeholder="Parolingizni kiriting" /> <br />
-        <button className="btn-submit">Kirish</button>
+        <button className="btn-submit" htmlType="submit" disabled={wait}>
+          Kirish
+        </button>
       </form>
     </div>
   );
