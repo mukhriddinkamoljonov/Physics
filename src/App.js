228865aca55ee9e22af591170d6f7889053d18f3
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-22 22:30:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Login from "./Components/Login/login";
 import Navbar from "./Components/Navbar/Navbar";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
